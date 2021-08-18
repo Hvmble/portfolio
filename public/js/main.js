bd8fc6c06faa6af98ab0c709f3405c7ab0e98b24
Mauricio Montoya <mmontoyaabadia@gmail.com>
2021-08-17 23:41:35 -0500

    Plantilla Beta del portfolio, sujeta a cambios en la seccion proyectos y skills

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-/*===== MENU SHOW =====*/ 
+/*=== Menu Show === */ 
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
@@ -11,21 +11,21 @@ const showMenu = (toggleId, navId) =>{
 }
 showMenu('nav-toggle','nav-menu')
 
-/*===== ACTIVE AND REMOVE MENU =====*/
+/*=== Active and Remove Menu === */
 const navLink = document.querySelectorAll('.nav__link');   
 
 function linkAction(){
-  /*Active link*/
-  navLink.forEach(n => n.classList.remove('active'));
-  this.classList.add('active');
-  
-  /*Remove menu mobile*/
-  const navMenu = document.getElementById('nav-menu')
-  navMenu.classList.remove('show')
+    /*Active link*/
+    navLink.forEach(n => n.classList.remove('active'));
+    this.classList.add('active');
+
+    /*Remove menu mobile*/
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
-/*===== SCROLL REVEAL ANIMATION =====*/
+/*=== Scroll Animation ===*/
 const sr = ScrollReveal({
     origin: 'top',
     distance: '80px',
@@ -33,29 +33,44 @@ const sr = ScrollReveal({
     reset: true
 });
 
-/*SCROLL HOME*/
+/*Scroll Home*/
 sr.reveal('.home__title',{}); 
 sr.reveal('.button',{delay: 200}); 
 sr.reveal('.home__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
 
-/*SCROLL ABOUT*/
+/*Scroll About*/
 sr.reveal('.about__img',{}); 
 sr.reveal('.about__subtitle',{delay: 400}); 
 sr.reveal('.about__text',{delay: 400}); 
 
-/*SCROLL SKILLS*/
+/*Scroll Skills*/
 sr.reveal('.skills__subtitle',{}); 
 sr.reveal('.skills__text',{}); 
-sr.reveal('.skills__data',{interval: 200}); 
+sr.reveal('.skills__container-card',{interval: 200}); 
 sr.reveal('.skills__img',{delay: 600});
 
-/*SCROLL WORK*/
+/*Scroll Work*/
 sr.reveal('.work__img',{interval: 200}); 
 
-/*SCROLL CONTACT*/
-sr.reveal('.contact__input',{interval: 200}); 
+/*Scroll Contact*/
+sr.reveal('.contact__input',{interval: 200});
 
 
-
+/* Typed */
+const typed = new Typed('.typed', {
+	
+	stringsElement: '#text-strings', 
+	typeSpeed: 75,
+	startDelay: 300, 
+	backSpeed: 60, 
+	smartBackspace: true,
+	shuffle: false, 
+	backDelay: 900, 
+	loop: true, 
+	loopCount: false, 
+	showCursor: true, 
+	cursorChar: '|', 
+	contentType: 'html', 
+});
 
